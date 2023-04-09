1cbd9c7f1aeadf7173f0c35697f6db0dd1067181
Алена Павлова <anapavlova1397@gmail.com>
2023-04-09 18:17:43 +0500

    added some new code in css and cart-icon

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import { FaShoppingCart } from "react-icons/fa";
+
 
 export default function Header() {
   return (
@@ -6,6 +8,7 @@ export default function Header() {
         <div className='nav_Container'>
             <span className='logo'>Candels store</span>
             <ul className='nav'>
+                <li><FaShoppingCart className='cart_logo'/></li>
                 <li>О нас</li>
                 <li>Контакты</li>
                 <li>Кабинет</li>
--- src/Components/Item.js
+++ src/Components/Item.js
@@ -6,7 +6,7 @@ export class Item extends Component {
             <div className='item'>
                 <img src={this.props.item.image} alt='candel' width={300} height={400}/>
                 <h2>{this.props.item.name}</h2>
-                <p>{this.props.item.description}</p>
+                <p>{this.props.item.description.substring(0,100)}</p>
                 <b>{this.props.item.price}.00 $</b>
                 <div className='add_to_cart'>+</div>
             </div>
